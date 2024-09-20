da65aab8e4fec6c91c14bbcd54813d4047287b92
Svitlana <svbeta777@gmail.com>
2024-09-20 18:09:28 +0200

    completed tasks 5-8

---
+++ part2_2/task_7.py
@@ -0,0 +1,9 @@
+a = 'I was'
+b = 'born'
+c = 'this way'
+a = input()
+b = input()
+c = input()
+print (a)
+print (b)
+print (c)
---
+++ part2_2/task_8.py
@@ -0,0 +1,9 @@
+a = "Hello"
+b = "it's"
+c = "me"
+a = input()
+b = input()
+c = input()
+print (c)
+print (b)
+print (a)
