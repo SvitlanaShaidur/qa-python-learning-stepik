1b4a9db91fbf15b917c94db39a7a6fb9e485d11c
Svitlana <svbeta777@gmail.com>
2024-10-11 17:48:14 +0200

    Description of this task was added Variables were excluded

--- part2_5/task2_5_9.py
+++ part2_5/task2_5_9.py
@@ -1,9 +1,7 @@
-num = int(input())
-d1 = num //1000 %  10
-d2 = num // 100 % 10
-d3 = num // 10 % 10
-d4 = num % 10
+# На вход программе подаётся положительное четырёхзначное целое число.
+# Программа должна вывести текст в соответствии с условием задачи
 
+d1, d2, d3, d4 = input()
 print('The digit in the thousands position is', d1)
 print('The digit in the hundreds position is', d2)
 print('The digit in the tens position is', d3)
