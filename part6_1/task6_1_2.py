2e6b7e9e84de5d4a90c293f1c40667468070e9fa
Svitlana <svbeta777@gmail.com>
2024-11-08 11:56:28 +0100

    Fix for 6_1_2 was done

--- part6_1/task6_1_2.py
+++ part6_1/task6_1_2.py
@@ -1,4 +1,4 @@
 # Определите, через какое время (в часах) старушки встретятся, если расстояние между ними равно S км.
-s, v1, v2 = float(input()), float(input()), float(input())
-t = s / (v1 + v2)
-print(t)
+distance, speed1, speed2 = float(input()), float(input()), float(input())
+time = distance / (speed1 + speed2)
+print(time)
