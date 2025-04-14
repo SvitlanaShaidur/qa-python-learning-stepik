2570eb0f3601ba19dd0586e34cfbd77835714fad
Svitlana <svbeta777@gmail.com>
2025-04-14 15:16:58 +0200

    All tasks from 11_6 were done

---
+++ part11_6/task11_6_2.py
@@ -0,0 +1,11 @@
+# На вход программе подаётся строка текста, содержащая различные натуральные числа.
+# Вам необходимо переставить максимальный и минимальный элементы местами и вывести изменённую строку.
+# Примечание. Используйте подходящие встроенные функции и списочные методы.
+
+nums = input().split()
+for i in range(len(nums)):
+    nums[i] = int(nums[i])
+min_num = nums.index(min(nums))
+max_num = nums.index(max(nums))
+nums[min_num], nums[max_num] = nums[max_num], nums[min_num]
+print(*nums)
