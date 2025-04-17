0f06cfc60fcdf0e5158cedb4904f3ce1ffcace80
SvitlanaShaidur <svbeta777@gmail.com>
2025-04-17 10:29:35 +0200

    Update task11_6_2.py
    Optimised task11_6_2.py

--- part11_6/task11_6_2.py
+++ part11_6/task11_6_2.py
@@ -2,9 +2,11 @@
 # Вам необходимо переставить максимальный и минимальный элементы местами и вывести изменённую строку.
 # Примечание. Используйте подходящие встроенные функции и списочные методы.
 
-nums = input().split()
-for i in range(len(nums)):
-    nums[i] = int(nums[i])
+# nums = input().split()
+# for i in range(len(nums)):
+#    nums[i] = int(nums[i])
+
+nums = [int(i) for i in input().split()] 
 min_num = nums.index(min(nums))
 max_num = nums.index(max(nums))
 nums[min_num], nums[max_num] = nums[max_num], nums[min_num]
