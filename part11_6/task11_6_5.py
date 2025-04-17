f4500c23543fb4ef2d078bc53f631303f72a8b5e
SvitlanaShaidur <svbeta777@gmail.com>
2025-04-17 10:40:31 +0200

    Update task11_6_5.py
    Optimised task11_6_5.py

--- part11_6/task11_6_5.py
+++ part11_6/task11_6_5.py
@@ -2,9 +2,11 @@
 # Из данной строки формируется список чисел.
 # Напишите программу, которая сортирует и выводит данный список сначала по возрастанию, а затем по убыванию.
 
-text = input().split()
-for i in range(len(text)):
-    text[i] = int(text[i])
+# text = input().split()
+# for i in range(len(text)):
+#     text[i] = int(text[i])
+
+text = [int(i) for i in input().split()]
 text.sort()
 print(*text)
 text.sort(reverse=True)
