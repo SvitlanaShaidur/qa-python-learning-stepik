a97c624b91c004981869acff6251960ead89a347
SvitlanaShaidur <svbeta777@gmail.com>
2025-04-28 08:43:38 +0200

    Fix  for  task13_5_1.py

--- part13_5/task13_5_1.py
+++ part13_5/task13_5_1.py
@@ -4,9 +4,7 @@
 
 
 def is_valid_triangle(side1, side2, side3):
-    if (side1 + side2 > side3) and (side1 + side3 > side2) and (side2 + side3 >side1):
-        return True
-    else:
-        return False
+    return (side1 + side2 > side3) and (side1 + side3 > side2) and (side2 + side3 >side1)
+        
 a, b, c = int(input()), int(input()), int(input())
 print(is_valid_triangle(a, b, c))
